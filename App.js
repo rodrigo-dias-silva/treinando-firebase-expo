ff5b5650c08a903025f70f6fd98e92f80429f762
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-10-14 05:12:23 -0300

    criaçao do projeto com expo, install de dependencias

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from './src/firebaseConnection';
 
 export default function App() {
+  const [nome, setNome] = useState('Buscando...');
+  const [idade, setIdade] = useState('');
+
+  useEffect(() => {
+
+    async function dados() {
+
+      // Olheiro da database elemento ".on"
+      // await firebase.database().ref('name').on('value', (snapshot) => {
+      //   setNome(snapshot.val());
+      // })
+
+      // Once busca uma unica vez no banco de dados ".once"
+      // await firebase.database().ref('name').once('value', (snapshot) => {
+      //   setNome(snapshot.val());
+      // })
+
+      await firebase.database().ref('user/1').on('value', (snapshot) => {
+        setNome(snapshot.val().name);
+        setIdade(snapshot.val().age);
+      })
+    }
+
+    dados();
+
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="inverted" />
+      <Text style={styles.text}>Olá {nome}</Text>
+      <Text style={styles.text}>Você tem {idade} anos</Text>
     </View>
   );
 }
@@ -13,8 +44,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121214',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff'
+  }
 });
